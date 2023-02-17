74fe0ac64016f29f554e3fb16c982ec288d667f9
stephen842 <ugotachisomstephen@gmail.com>
2023-02-16 20:37:11 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+/**
+ * main - this will print the a sentence
+ *
+ * return: 0 when it is succesful
+ */
+
+int main(void)
+{
+     printf("with proper grammar, but the outcome is a piece of art,\n");
+     return (0);
+}
