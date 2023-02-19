d06c0c10a7c5919ba835bcdf8b42ab87d69b1964
stephen842 <ugotachisomstephen@gmail.com>
2023-02-19 09:44:58 -0800

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function goes here
+ *
+ * Return: 0 as always
+ */
+
+int main(void)
+{
+	for (i = "a" ; i <= "z" ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
