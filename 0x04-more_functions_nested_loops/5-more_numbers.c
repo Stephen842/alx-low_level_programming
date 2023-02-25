288ee9f933a7e7a0d2ff2272e29d996ee9b09398
stephen842 <ugotachisomstephen@gmail.com>
2023-02-24 16:48:02 -0800

    function that draws a straight line in the terminal.

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the numbers, from 0 to 14
+ * Return: 0 as usual
+ */
+
+void more_numbers(void)
+{
+	int b, c;
+
+		for (b = 1 ; b <= 10 ; b++)
+		{
+			for (c = 0 ; c <= 14 ; c++)
+			{
+				if (c >= 10)
+				{
+					_putchar('1');
+					_putchar(c % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+}
