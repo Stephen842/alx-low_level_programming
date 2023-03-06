2702001dedca5f49cbcd15e765a1b240df3bb269
stephen842 <ugotachisomstephen@gmail.com>
2023-03-06 15:26:51 -0800

    function that print the size of an array

---
+++ stephen/14-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - to print the size of an array
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int array[10];
+
+	printf("sizeof array: %lu\n", sizeof(array));
+	return (0);
+}
