901a00c41040f5b13e6a85319f8ada4232e773e5
stephen842 <ugotachisomstephen@gmail.com>
2023-03-15 07:18:49 -0700

    i made an adjustment by changing the error in lowercase to capitalization in it first letter

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - give the minimum number of a coins to make change for a sum.
+ * @argc: function paramter
+ * @argv: function parameter
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+
+	int cents;
+	int coin = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents = cents - 25;
+		else if (cents >= 10)
+			cents = cents - 10;
+		else if (cents >= 5)
+			cents = cents - 5;
+		else if (cents >= 2)
+			cents = cents - 2;
+		else if (cents >= 1)
+			cents = cents - 1;
+		coin++;
+		printf("%d\n", coin);
+	}
+	return (0);
+}
