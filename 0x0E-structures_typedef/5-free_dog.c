f1e08c761b446c7fbf0347f105601a7729953a5c
stephen842 <ugotachisomstephen@gmail.com>
2023-03-21 08:33:40 -0700

    A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog to set free
+ * Return: 0 if successful
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
