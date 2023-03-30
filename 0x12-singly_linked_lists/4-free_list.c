373b454fa5cf918799031683a95f0a7dd956a0a1
stephen842 <ugotachisomstephen@gmail.com>
2023-03-30 09:10:13 -0700

    Write a function that adds a new node at the end of a list_t list.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - function that frees a list_t list.
+ * @head: function parameter
+ * Return: 0 if successful
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
