56b5d09d19a8d063b39b8d09e3ab152aec830793
stephen842 <ugotachisomstephen@gmail.com>
2023-04-05 16:52:03 -0700

    a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,7 +10,8 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *current = head, *temp;
+	const listint_t *current = head;
+	const listint_t *temp;
 
 	while (current != NULL)
 	{
