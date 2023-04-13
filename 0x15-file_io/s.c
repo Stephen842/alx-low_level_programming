9ea7c8e3c2669be92a85bf76a0f655402a051c93
stephen842 <ugotachisomstephen@gmail.com>
2023-04-13 06:35:19 -0700

    function that create a file

--- 0x15-file_io/s.c
+++ 0x15-file_io/s.c
@@ -12,25 +12,29 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, a, length = 0;
+	int fd;
+	int a;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
+	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
 	{
-		for (length = 0; text_content[length];)
-			length++;
+		a = write(fd, text_content, strlen(text_content));
+		if (a == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	a = write(fd, text_content, length);
-
-	if (fd == -1 || a == -1)
-		return (-1);
-
 	close(fd);
+	a = chmod(filename, S_IRUSR | S_IWUSR);
+	if (a == -1)
+		return (-1);
 
 	return (1);
-
 }
