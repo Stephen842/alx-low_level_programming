3d5202bb8272968ff9b299397b6f8685e95e7bc5
Stephen842 <ugotachisomstephen@gmail.com>
2023-05-04 07:46:54 -0700

    i just debug using the betty doc style

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_bit - func that returns the val of a bit given at an index
+ * @n: function parameter indicating the number to search
+ * @index: function parameter indicating the index of the number
+ * Return: the value of a bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	i
+}
